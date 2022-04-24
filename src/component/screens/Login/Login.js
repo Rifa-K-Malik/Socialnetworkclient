9197dc8dbce28a4bfbc262ed5a40fb8f277a0fcf
Rifa-K-Malik <malikrif07@gmail.com>
2022-04-24 12:54:15 +0530

    SignUp SignIN UI

--- src/component/screens/Login/Login.js
+++ src/component/screens/Login/Login.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='login'>
+      <div className='card auth-card input-field'>
+        <h3 className='h2 grey-text text-darken-3'>SocialNetwork</h3>
+        <input id="email" type="email"  placeholder='email' />
+        <input id="password" type="password"  placeholder='password' />
+        <button className="btn waves-effect waves-light loginbtnn" type="submit" name="action">SignIn</button>
+        <h6>Don't Have an Account? <Link to ="/signin"><span className='span-udline'>SIGNUP</span></Link></h6>
+      </div>
+    </div>
   )
 }
 
--- src/component/screens/signup/Signup.js
+++ src/component/screens/signup/Signup.js
@@ -1,30 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Signup() {
     return (
-        <div>
-            <div class="row">
-                <div class="col s12 m6">
-                    <div class="card blue-grey darken-1">
-                        <div class="card-content white-text">
-                            <div class="input-field">
-                                <input id="last_name" type="text" class="validate" />
-                                <label for="last_name">Name</label>
-                            </div>
-                            <div class="input-field">
-                                <input id="last_name" type="text" class="validate" />
-                                <label for="last_name">Email</label>
-                            </div>
-                            <div class="input-field">
-                                <input id="last_name" type="text" class="validate" />
-                                <label for="last_name">Password</label>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </div>
+<div className='signup'>
+      <div className='card auth-card input-field'>
+        <h3 className='h2 grey-text text-darken-3'>SocialNetwork</h3>
+        <input id="username" type="text"  placeholder='username' />
+        <input id="email" type="email"  placeholder='email' />
+        <input id="password" type="password"  placeholder='password' />
+        <button className="btn waves-effect waves-light signupbtnn" type="submit" name="action">SignUp</button>
+        <h6>Already Have an Account ? <Link to ="/signin"><span className='span-udline'>SIGNIN</span></Link></h6>
+      </div>
+    </div>
     )
 }
 
